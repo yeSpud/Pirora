cd048a37c01c410c22ef21d0c72bb21dfefd77c2
Spud <jeffrypig23@gmail.com>
2023-01-17 17:15:44 -0900

    Initial setup

---
+++ aurora.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+
+'''
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+'''
