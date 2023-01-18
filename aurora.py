8aac4ac65ee3aec128a0c7f86841c9f2af9bc4a9
Spud <jeffrypig23@gmail.com>
2023-01-17 20:09:24 -0900

    Fixed a time formatting bug

--- aurora.py
+++ aurora.py
@@ -32,8 +32,9 @@ threeday = soup.find(id="db-data-3-day")
 # twentysevenday_json = json.loads(twentysevenday.text)
 threeday_json = json.loads(threeday.text)
 
+# Get the current UTC time and round it to the nearest 3rd hour (so 00:00:00, 03:00:00, 06:00:00...).
 now = datetime.now(timezone.utc)
-formattedTime: str = f"{now.year:04d}-{now.month:02d}-{now.day:02d} {now.hour:02d}:00:00"
+formattedTime: str = f"{now.year:04d}-{now.month:02d}-{now.day:02d} {int(now.hour/3)*3:02d}:00:00"
 
 kp_value = 0.0
 for entry in threeday_json:
@@ -41,16 +42,18 @@ for entry in threeday_json:
         kp_value = float(entry["kp"])
         break
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN, GPIO.OUT)
-GPIO.setwarnings(False)
 p = GPIO.PWM(PIN, 60)  # channel=12 frequency=60Hz
 if kp_value >= THRESHOLD:
-    #GPIO.output(PIN, GPIO.HIGH)
+    # pass
+    # GPIO.output(PIN, GPIO.HIGH)
     p.start(BRIGHTNESS)
 
 else:
-    #GPIO.output(PIN, GPIO.LOW)
+    # pass
+    # GPIO.output(PIN, GPIO.LOW)
     p.stop()
 
 # Exit with 0 marking success.
